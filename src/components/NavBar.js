238355b40535fe075e808204558856d813d4450b
Mohd Sameer <83845532+sameer3134@users.noreply.github.com>
2022-01-10 00:43:04 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export class NavBar extends Component {
             <>
            
             <div><nav className ="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <Link  className ="navbar-brand mx-2" to="/">National News</Link>
+            <Link  className ="navbar-brand mx-2" to="/top">National News</Link>
             <button className ="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className ="navbar-toggler-icon"></span>
             </button>
@@ -17,7 +17,7 @@ export class NavBar extends Component {
                 
                 <li className ="nav-item "> <Link  className ="nav-link" to="/business">Business</Link> </li>
                 <li className ="nav-item "> <Link  className ="nav-link" to="/entertainment">Entertainment</Link> </li>
-                <li className ="nav-item "> <Link  className ="nav-link" to="/general">General</Link> </li>
+                <li className ="nav-item "> <Link  className ="nav-link" to="/top">General</Link> </li>
                 <li className ="nav-item "> <Link  className ="nav-link" to="/health">Health</Link> </li>
                 <li className ="nav-item "> <Link  className ="nav-link" to="/science">Science</Link> </li>
                 <li className ="nav-item "> <Link  className ="nav-link" to="/technology">Technology</Link> </li>
