f5692e9b3bef1bacd7e4cd472d9cc5029764f9fb
Mohd Sameer <83845532+sameer3134@users.noreply.github.com>
2022-01-10 00:44:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,12 +30,12 @@ export default class App extends Component {
 
       />
         <Switch>
-          <Route exact path="/"> <News setProgress={this.setProgress}  key='general'  country='in' category='general'/> </Route>
+          <Route exact path="/top"> <News setProgress={this.setProgress}  key='top'  country='in' category='general'/> </Route>
           <Route exact path="/home"> <News setProgress={this.setProgress}  key='home'  country='in' category='home'/> </Route>
           <Route exact path="/business"> <News setProgress={this.setProgress}  key='business'   country='in' category='business'/> </Route>
           <Route exact path="/technology"> <News setProgress={this.setProgress}  key='technlogy'   country='in' category='technology'/> </Route>
           <Route exact path="/sport"> <News setProgress={this.setProgress}  key='sport'   country='in' category='sport'/> </Route>
-          <Route exact path="/general"> <News setProgress={this.setProgress}  key='general'   country='in' category='general'/> </Route>
+          <Route exact path="/general"> <News setProgress={this.setProgress}  key='top'   country='in' category='general'/> </Route>
           <Route exact path="/health"> <News setProgress={this.setProgress}  key='health'   country='in' category='health'/> </Route>
           <Route exact path="/entertainment"> <News setProgress={this.setProgress}  key='entertainment'   country='in' category='entertainment'/> </Route>
           <Route exact path="/science"> <News setProgress={this.setProgress}  key='science'   country='in' category='science'/> </Route>
